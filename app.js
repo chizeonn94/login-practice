78d576db844eef74c0fa1db4081ebb920e90fdec
chizeonn94 <59093532+chizeonn94@users.noreply.github.com>
2021-09-13 16:41:42 +0900

    hard coding login page

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-const http = require("http");
-const app = http.createServer((req, res) => {
-  console.log(req.url);
-  res.writeHead(200, { "content-Type": "text/html; charset=utf-8 " });
-  if (req.url === "/") {
-    res.end("여기는 루트입니다.");
-  } else if (req.url === "/login") {
-    res.end("여기는 로그인화면입니다.");
-  }
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  console.log("hi");
+  res.send(`
+    <!DOCTYPE html>
+        <html lang="ko">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+            여기는 루트입니다.
+        </body>
+        </html>
+  `);
 });
-app.listen(3001, () => {
-  console.log("http로 가동된 서버입니다.");
+app.get("/login", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <input type="text" placeholder="아이디"/><br /><input type="text" placeholder="비번"/><br />
+        <button>로그인</button>
+    </body>
+    </html>
+
+  `);
 });
-// const express = require("express");
-// const app = express();
-// app.get("/", (req, res) => {
-//   console.log("hi");
-//   res.send("여기는 루트 화면입니다.");
-// });
-// app.get("/login", (req, res) => {
-//   res.send("여기는 로그인화면입니다. -지선");
-// });
-// app.listen(3000, () => {
-//   console.log("서버가동");
-// });
+app.listen(3000, () => {
+  console.log("서버가동");
+});
