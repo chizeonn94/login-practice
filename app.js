dd36000fed93bb7eba1882d6aaeef4045f645951
chizeonn94 <59093532+chizeonn94@users.noreply.github.com>
2021-09-13 15:31:25 +0900

    Update app.js
    modified sending message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("여기는 루트 화면입니다.");
 });
 app.get("/login", (req, res) => {
-  res.send("여기는 로그인화면입니다.");
+  res.send("여기는 로그인화면입니다. -지선");
 });
 app.listen(3000, () => {
   console.log("서버가동");
