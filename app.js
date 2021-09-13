23b16b4550f892841a7e16280e725b88c41bc3b2
chizeonn94 <59093532+chizeonn94@users.noreply.github.com>
2021-09-13 15:22:51 +0900

    [app.js]sending message
    login, root

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const app = express();
-app.listen(3000, ()=>{console.log('서버가동')})
+app.get("/", (req, res) => {
+  console.log("hi");
+  res.send("여기는 루트 화면입니다.");
+});
+app.get("/login", (req, res) => {
+  res.send("여기는 로그인화면입니다.");
+});
+app.listen(3000, () => {
+  console.log("서버가동");
+});
