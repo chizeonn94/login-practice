27fe97705b48a095110373e092e7ea7d7800d37c
junyoung desktop <chizeonn94@gmail.com>
2021-09-21 22:36:41 +0900

    arranged folder layout

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+
+//앱세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+const home = require("./src/routes/home");
+app.use("/", home); //use는 미들웨어를 등록해주는 메서드.
+
+module.exports = app;
+
+
