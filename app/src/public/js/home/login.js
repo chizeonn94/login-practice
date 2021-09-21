8a771d4b5756d0db51071ab87c5030871b96627f
junyoung desktop <chizeonn94@gmail.com>
2021-09-22 00:25:14 +0900

    login functionality

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,4 +17,7 @@ function login(){
         },
         body: JSON.stringify(req)
     })
+        .then((res)=>res.json())
+        .then((res)=>console.log(res))
+
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+  id:["lea", "chris", "josh"],
+  psword:["1234", "aaa", "jjj"]
+}
+
 const output = {
   home: (req, res) => {
     res.render("home/index");
@@ -12,7 +17,17 @@ const output = {
 
 const process = {
   login : (req, res) => {
-    console.log('bbb',req.body);
+     console.log('bbb',req.body);
+    const id = req.body.id;
+    const psword = req.body.psword;
+    if(users.id.includes(id)){
+      const idx = users.id.indexOf(id)
+      if(users.psword[idx]===psword){
+        return res.json({success:true})
+      }
+    }
+    console.log('hihi')
+    return res.json({success:false, message:'failed login'})
   },
 }
 
