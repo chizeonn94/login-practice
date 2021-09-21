047aa179ae1ab7e5ffaa9647c2fa5223c828f0db
junyoung desktop <chizeonn94@gmail.com>
2021-09-21 22:50:35 +0900

    added js file for front

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const app = express();
 //앱세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 const home = require("./src/routes/home");
 app.use("/", home); //use는 미들웨어를 등록해주는 메서드.
